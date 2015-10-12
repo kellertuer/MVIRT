c7f62c93512ad811e5709bbbe3ddf94d95d8b854
Ronny Bergmann <bergmann@imgpro-pc10.mathematik.uni-kl.de>
2015-10-12 17:57:01 +0200

    adds the prototypical manifold class and the Sphere S1

---
+++ manifolds/S1.m
@@ -0,0 +1,219 @@
+classdef S1 < manifold & handle
+    % The manifold of the circle or S1
+    %
+    % ---
+    % ManImRes 1.0, R. Bergmann ~2014-10-22
+    properties
+        type = 'S1';
+        ItemSize = 1;
+    end
+    
+    methods
+        function q = exp(~,p,v,t)
+            % exp(p,v) - Exponential map at the point p with respect to v in
+            % TpM on S1.
+            %
+            % INPUT
+            %   p : a point or set of points on the manifold S1
+            %   v : a point or set of point in the tangential spaces TpM
+            %
+            % OUTPUT
+            %   q : resulting point(s) on S1
+            % ---
+            % ManImRes 1.0, R. Bergmann ~ 2014-10-19
+            if nargin<4
+                t=1;
+            end
+            if ~isscalar(t)
+                size_p = size(p);
+                assert(all(size_p(2:end)==size(t)),'t should either be a scalar or have the same size as p'); 
+            end
+            assert(all(size(p)==size(v)),'p and q have to be of same size');
+            q = symMod(v+p,2*pi);
+        end
+        function v = log(~,p,q)
+        % log(q,p) - Inverse Exponential Map at p of q.
+        %
+        % INPUT
+        %    p : point or set of (column) points indicating the
+        %        tangential base points
+        %    q : point(s) on S1 (i.e. [-pi,pi) being put into the
+        %        tangential plane at their corresponding p
+        %
+        % OUTPUT
+        %    v : points on the tangential plane at point(s) p
+        % ---
+        % ManImRes 1.0, R. Bergmann ~ 2014-10-22
+            assert(all(size(p)==size(q)),'p and q have to be of same size');
+            v = symMod(q-p,2*pi);
+        end
+        function d = dist(~,p,q)
+        % dist(a,b) computes the length of the smaller arc of a,b on S1
+        %   also works for arbitrary sized array a and b componentwise
+        %    INPUT
+        %        p,q    : 2 point sets on the S1 = [-pi,pi)
+        %
+        %    OUTPUT
+        %        d      : lengths of the shorter arcs between a and b
+        % ---
+        % ManImRes 1.0, R. Bergmann ~ created 2013-10-25 | 2014-10-22
+            if iscolumn(p)
+                p_ = p';
+            else
+                p_ = p;
+            end
+            if iscolumn(q)
+                q_ = q';
+            else
+                q_ = q;
+            end
+            if (any(size(p_)~=size(q_)))
+                error('Distances can only be computed for equal length vectors p and q');
+            end
+            d = squeeze(abs(symMod(p_-q_,2*pi))); %squeeze first dimension if both are not vectors
+        end
+        function x = proxad(this,varargin)
+            % proxad(f,lambda,w) compute the [PROX]imal mapping for
+            %       [A]bsolute ([C]yclic) [D]ifferences for each column of f with
+            %       respect to weight w, where the length of w has to be the same as
+            %       the column length (rows) of f. Unknown (NaN) entries of f are
+            %       computed if possible, i.e. if they are the only unknown value in
+            %       one column.
+            % proxad(problem)
+            %       Call the same function but with a struct containing both
+            %       the mandatory (f,lambda,w) and optional parameters (see
+            %       below)
+            %
+            %
+            % INPUT
+            %    f      : each column of f is threatened as one data set
+            %             if f is a row vector, it isjust threated as one data set
+            %    lambda : usual value of the proximum
+            %    w      : weights corresponding to each column of f and with sum 0
+            %
+            %   OUTPUT
+            %    x      : proximal mappings
+            %
+            % OPTIONAL PARAMETERS
+            % 'RegMask'     : ([]) Specify a binary mask for values affected by
+            %                  (1) they are regularized, i.e. moved (or active, say)
+            %                  (0) fixed. RegMask is of the same size
+            %                  the data point set, i.e. the number of rows.
+            %
+            %
+            % See also:
+            %    cppa_ad_1D, cppa_ad_2D, cppa_ad_nD
+            % ---
+            % ManImRes ~ R. Bergmann ~ 2014-03-23 | 2014-11-29
+            if length(varargin)==1 && isstruct(varargin{1}) %struct case - less checks
+                vars = varargin{1};
+                assert(all(isfield(vars,{'f','lambda','w'})),...
+                    'Not all required parameters given in the struct');
+                f = vars.f;
+                [~,l,d] = size(f); %number of sets (l) and length of each data set (d)
+                if ~isfield(vars,'RegMask');
+                  regMask = ones(l,d); %move all
+                else
+                  regMask = vars.RegMask;
+                end
+                w = vars.w;
+            else
+                ip = inputParser;
+                addRequired(ip,'f');
+                addRequired(ip,'lambda');
+                addRequired(ip,'w');
+                addParameter(ip, 'RegMask', []);
+                parse(ip, varargin{:});
+                vars = ip.Results;
+                if isrow(vars.w) %if w is a single row -> use as column.
+                    vars.w = vars.w';
+                end
+                if sum(vars.w)~=0
+                    error('The sum of the weights w has to be zero.');
+                end
+                f = vars.f;
+                if (size(f,2)==1) && (length(size(vars.f))==2) %one column data
+                    f = f';
+                end
+                if (isrow(f)) % 1xd - ein Datensatz
+                    f = permute(f,[3,1,2]); %1x1xd
+                elseif (length(size(f))==2) %lxd
+                    f = permute(f,[3,1,2]); % to 1xlxd
+                end
+                [k,l,d] = size(f); %number of sets (l) and length of each data set (d)
+                if k~=this.ItemSize
+                    error('No S1 data!');
+                end
+                if length(w)~=d
+                    error(['Wrong amount of weights w (length: ',...
+                    num2str(length(w)),') for data f (length: ',num2str(d),').']);
+                end
+                regMask = vars.RegMask;
+                if numel(regMask)>0 % Mask given
+                    if any(size(regMask)~=[size(f,2),size(f,3)])
+                        warning(['Size of the regularization mask (',...
+                            num2str(size(regMask)),') does not fit the size of f (',...
+                            num2str([l,d]),'). Hence it is ignored.']);
+                        regMask = ones(l,d); %move all
+                    end
+                else
+                    regMask = ones(l,d); %move all
+                end
+            end
+            %% Divide f into 3 sets:
+            % those, where it is possible to inpaint
+            inpaintcols = permute(sum(isnan(f(1,:,:)),3)==1,[2,1]);
+            l1 = sum(inpaintcols);
+            % those that do not contain NaNs
+            proxcols = permute(sum( isnan(f(1,:,:)),3)==0,[2,1]);
+            l2 = sum(proxcols);
+            % and the rest is not handled at all
+            x = zeros(1,l,d);
+            % third: Those with more than one NaN -> keep them unchanged
+            if (l1+l2)<l
+                x(:,~(inpaintcols|proxcols),:) = f(:,~(inpaintcols|proxcols),:);
+            end
+            %
+            %% Inpaint the first case
+            if (l1>0) % there are possible values to pe intainted - not yet debugged
+                    W = permute(repmat(w,[1,l1]),[2,1]);
+                    prex = permute(f(1,inpaintcols,:),[2,3,1]);
+                    %missing column entries, i.e. rows of '
+                    [missingW,~] = find(isnan(prex'));
+                    divW = diag(W(1:l1,missingW));
+                    predot = prex.*W;
+                    predot(isnan(predot))=0;
+                    prex = prex.'; %work on rows
+                    prex(isnan(prex)) = symMod(-sum(predot,2)./(divW),2*pi);
+                    prex = prex.'; %after filling, get back
+                    x(1,inpaintcols,:) = prex;
+            end
+            %% prox the second case
+            % scalar product <f,w> for each row
+            if (l2>0) % there are proxes...
+                sp = symMod( dot(repmat(w,[1,l2]),permute(f(:,proxcols,:),[3,2,1])), 2*pi);
+                % By Theorem 3.5 of BSW14 - ommitting the + case for
+                s = sign(sp);
+                % elementwise minima, denoted m in Thm 3.5
+                %
+                % If lambda is a number - it is applied to all elements, if it is a
+                % vector, it has to mach the dimensions (1,m)
+                nwsq = sum(permute(regMask(proxcols,:),[2,1]).*repmat(w,[1,l2]).^2,1);
+                mins = min(ones(1,l2).*vars.lambda, abs(sp)./nwsq);
+                % Multiply elementwise the sign (s) - the rest works the same as
+                % usual mult
+                x(:,proxcols,:) = symMod(f(:,proxcols,:) - permute(regMask(proxcols,:),[3,1,2]).*permute(w*(s.*mins),[3,2,1]),2*pi); % f + s*m*w for each row of f
+            end
+            % Rearrange if input was squeezed somehow
+            if (size(vars.f,2)==1) && (length(size(vars.f))==2) %one column data
+                x = permute(x,[3,1,2]);
+            end
+            if (isrow(vars.f)) || (length(size(vars.f))==2) %was mxd % 1xd - ein Datensatz
+                x = permute(x,[2,3,1]); % 1xmxd -> mxdx1
+            end
+        end
+        function fn = addNoise(~,f,sigma)
+            fn = symMod(f + sigma*randn(size(f)),2*pi);
+        end
+    end
+end
---
+++ manifolds/manifold.m
@@ -0,0 +1,193 @@
+classdef (Abstract) manifold < handle
+    % A Manifold.
+    % This class provides all generell functions available on manifolds and
+    % implements those, that are only based on these function interfaces
+    %
+    % Instances of manifolds have to provide these functions.
+    %
+    % PROPERTIES
+    %   type : a string indicating the type of manifold
+    %
+    % FUNCTIONS
+    % _Abstract, Static
+    %    exp(p,v)            : exponential map at p w.r.t v.
+    %    log(p,q)            : inverse exponential map of q at p
+    %    dist(p,q)           : distance of p and q on the manifold
+    % _Static
+    %    proxad(f,lambda,w)  : proximal mappinf w.r.t. absolute differences
+    %    addNoise(X,sigma)   : add Noise on the manifold to the signal/data
+    %                          X with standard deviation sigma
+    % _normal functions
+    %    proxDist(g,f,lambda) : proximal mapping of distance terms
+    %    mean(f,q)            : Karcher mean of the values f with inital q
+    %    midPoint(x,z)        : Compute the mid point between x and z.
+    %
+    % ---
+    % ManImRes 1.0, R. Bergmann ~ 2014-10-18 | 2015-01-25
+    
+    properties
+        useMex = true; %Whether or not to use mex-files in the manifold-functions
+    end
+    properties (Abstract)
+        type; % Type of manifold
+        ItemSize; % Data Item dimension, given as size(q), q from M; might differ from manifold dimensions
+    end
+    methods (Abstract)
+        % proxad(f,lambda,w,<options>)
+        %     perform a proximal mapping with respect to absolute differences
+        %     given by w of data f and parameter lambda on the manifold. Any
+        %     manifold should also provide the function as
+        % proxad(problem), where problem is a struct containing all necessary
+        %     parameters.
+        x = proxad(this,f,lambda,w,varargin);
+        % exponential map of v in TpM  at p on the manifold
+        q = exp(this,p,v);
+        % inverse exponential map of q at p on the manifold
+        v = log(this,p,q);
+        % distance between p and q on the manifold
+        d = dist(this,p,q);
+        % add noise w.r.t. manifold and standard deviation sigma to a
+        % (multidimensional) signal X
+        Y = addNoise(X,sigma)
+    end
+    methods
+        function x = proxDist(this,g,f,lambda)
+            % proxDist(g,f,lambda)
+            % Proximal step towards f from given data g with parameter
+            % lambda on an arbitrary manifold. This is the proximal map of
+            % the distance function squared with fixed g.
+            % INPUT
+            %  f,g    : data point( sets/columns )
+            %  lambda : stepsize towards f
+            % OUTPUT
+            %       x : result point( sets) of the proximal map
+            % ---
+            % ManImRes 1.0, R. Bergmann ~ 2014-10-19
+            if all(g(:) == f(:))
+                x=f;
+                return
+            end
+            t = this.dist(g,f);
+            if (size(t,1)==1) %column vector
+                t = shiftdim(t,1);
+            end
+            tS = size(t); tS = tS(tS>1);
+            v = this.log(g,f);
+            % repeat into all manifold dimensions
+            %first part produces singleton dimensions - as many as the
+            %manifold needs, last reproduces t in permute
+            t = repmat(... %permute singleton dimensions (as many as manifold dims) from last to first
+                    permute(t,[(length(size(f))-length(this.ItemSize)+1):(length(size(f))),1:(length(tS))]), ...
+                    [this.ItemSize(this.ItemSize>1),ones(1,length(tS))]);
+            x = this.exp(g, (lambda./(1+lambda)).*t.*v);
+        end
+        function x = mean(this,varargin)
+            % mean(f) calculates the mean of the input data with a gradient
+            % descent algorithm. This implementation is based on
+            %
+            % B. Afsari, Riemannian Lp center of mass: Existence,
+            %    uniqueness, and convexity,
+            %    Proc. AMS 139(2), pp.655-673, 2011.
+            %
+            % INPUT
+            %    f :  m x n Data points ([this.Itemsize,m,n]) to compute
+            %         m means of n points each, pp.
+            % OUTPUT
+            %    x :  m data points of the means calculated
+            %
+            % OPTIONAL
+            % 'Weights' : (1/n*ones([m,n]) 1xn or mxn weights for the mean
+            %            the first case uses the same weights for all means
+            % 'InitVal' : m Initial Data points for the gradient descent 
+            % 'MaxIterations': Maximal Number of Iterations
+            % 'Epsilon'      : Maximal change before stopping
+            %
+            %
+            % ManImRes 1.0, J. Persch 2015-07-24 | R. Bergmann 2015-07-30
+            ip = inputParser;
+            addRequired(ip,'f');
+            addParameter(ip,'Weights',NaN);
+            addParameter(ip,'InitVal',NaN);
+            addParameter(ip,'MaxIterations',50);
+            addParameter(ip,'Epsilon',10^-6);
+            parse(ip, varargin{:});
+            vars = ip.Results;
+            f = vars.f;
+            dims = size(f);
+            if length(dims) ~= length(this.ItemSize)+2
+                error('f wrong size');
+            end
+            % shift manDim in first dimension
+            f = reshape(f,[prod(this.ItemSize),dims(1+length(this.ItemSize):end)]);
+            con_dim = size(f);
+            m = con_dim(end-1);
+            n = con_dim(end);
+            if isnan(vars.Weights)
+                w = 1/n*ones(m,n);
+            elseif isvector(vars.Weights)
+                w = vars.Weights(:)';
+                if length(w) ~=n
+                    error('length(w) does not match data points');
+                end
+                w = repmat(w,m,1);
+            else
+                w = vars.Weights;
+                if any(size(w) ~= [m,n])
+                    error('dim w do not match data points');
+                end
+            end
+            % Resize w to fit to the Manifold
+            w = repmat(permute(w,[2+(1:length(this.ItemSize)),1,2]),[this.ItemSize,1,1]);
+            if isnan(vars.InitVal)
+                x = reshape(f,[prod(this.ItemSize),m,n]);
+                x = reshape(x(:,:,1),[this.ItemSize,m]);
+            else
+                x = vars.InitVal;
+                if (length(size(x))== length(this.ItemSize)) ...
+                        && (all(size(x) == this.ItemSize))
+                    x = repmat(x,[ones(1,length(this.ItemSize)),m]);
+                elseif any(size(x) ~= [this.ItemSize,m])
+                    error(['InitVal has to be of size [',num2str(this.ItemSize),'] or [',...
+                        num2str([this.ItemSize,m]),'].']);
+                end
+            end
+            if vars.Epsilon > 0
+                epsilon = vars.Epsilon;
+            else
+                warning('Epsilon should be larger than zero, set Epsilon to 10^-6')
+                epsilon = 10^-6;
+            end
+            if vars.MaxIterations > 0
+                iter = vars.MaxIterations;
+            else
+                warning('Iterations should be larger than zero, set Iterations to 100')
+                iter = 100;
+            end
+            f  = reshape(f, [this.ItemSize,m,n]);
+            x_old = x;
+            i = 0;
+            while (max(this.dist(x,x_old)) > epsilon && i < iter) || i == 0
+                x_old = x;
+                V = this.log(repmat(x,[ones(1,length(this.ItemSize)+1),n]),f);
+                V = V.*w;
+                V = sum(V,length(this.ItemSize)+2);
+                x = this.exp(x,V);
+                i= i+1;
+            end
+        end
+        function m = midPoint(this,x,z)
+            % m = midPoint(x,z)
+            %   Compute the (geodesic) mid point of x and z.
+            %
+            % INPUT
+            %    x,z : two point(sets) of manifold points
+            %
+            % OUTPUT
+            %      m : resulting mid point( sets)
+            %
+            % ---
+            % ManImRes 1.0, R. Bergmann ~ 2014-10-19 | 2015-01-29
+            m = this.exp(x, this.log(x,z)./2);
+        end
+    end
+end
