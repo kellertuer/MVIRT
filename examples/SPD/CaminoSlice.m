ab458114c39e9efed2f2fef0d7b92453d4ec6d48
Ronny Bergmann <mail@rbergmann.info>
2015-12-12 09:15:33 +0100

    adds a first SPD example loading a subset of the Camino dataset and its asy plots and images

---
+++ examples/SPD/CaminoSlice.m
@@ -0,0 +1,166 @@
+%
+% Camino Slice 28
+% ===============
+%
+% Denoising the Slice 28 of the Camino Data Set available at
+% http://cmic.cs.ucl.ac.uk/camino//index.php?n=Tutorials.DTI
+% (the file dt.Bdouble from step 5 used here as Caminodt.Bdouble)
+%
+% This file requires either a previous run, such that CaminoSlice28.mat is
+% prtesent in the Camino/ sub folder or that the Caminodt.Bdouble is provided
+% in the data/ folder to generate the CaminoSlice28.mat file.
+%
+% See also Section 5.2 and Figure 5.5 of
+%
+% R. Bergmann, M. Bacak, G. Steidl, A. Weinmann: A second order non-smooth
+%   variational model for restoring manifold-valued images, 2015.
+%
+% ---
+% Manifold Valued Image Restoration 1.0
+% R. Bergmann ~ 2015-04-01 | 2015-12-10
+start = pwd;
+if ~isempty(fileparts(which(mfilename)))
+    cd(fileparts(which(mfilename)));
+end
+run('../../initManImRes.m')
+%
+%
+%% Settings & Variables
+setDebugLevel('LevelMin',0);        % Minimal Value of all global Values
+setDebugLevel('LevelMax',1000);     % Maximal Value ...
+setDebugLevel('text',3);            % quite verbose text, but...
+setDebugLevel('IterationStep',1);   % only every Xth iteration
+setDebugLevel('WriteImages',1);     % 0: no file writing, 1: file writing
+setDebugLevel('LoadData',1);        % 0: generate new data 1: load existing data (if it exists), (other wise it is generated)
+setDebugLevel('WriteData',0);       % 0: do not write data to file 1: write data to file (overwrites last experiment data!)
+setDebugLevel('time',3);            % verbose time
+setDebugLevel('Figures',1);         % 0: no figure display, 1: figures are displayed
+setDebugLevel('logfile',1);         % 0: no logfile 1: logfile
+format compact
+%% Strings
+dataFolder = ['..',filesep,'..',filesep,'data',filesep];
+folder = ['examples',filesep,'SPD',filesep];
+% relative to folder
+resultsFolder = ['Camino',filesep];
+
+% Data details - ignored if only loading data
+sliceNum = 28;
+name = ['CaminoSlice',num2str(sliceNum)];
+dataName = ['CaminoSlice',num2str(sliceNum)];
+rangeX = 1:112;
+rangeY = 1:112;
+subRangeX = 28:87;
+subRangeY = 34:73;
+fn = [];
+
+%% Initialization
+if getDebugLevel('logfile')
+    clc
+    if exist([resultsFolder,name,'.log'],'file')
+        delete([resultsFolder,name,'.log'])
+    end
+    diary([resultsFolder,name,'.log']);
+    disp([' --- Logfile of Experiment ',name,' started ',datestr(datetime),' ---']);
+end
+if getDebugLevel('WriteData') % recreate data from original data and save
+    %% Load and reformate Data
+    % Open Camino Data file from the tutorial at http://cmic.cs.ucl.ac.uk/camino/index.php?n=Tutorials.DTI
+    if ~(exist([dataFolder,'Caminodt.Bdouble'],'file')==2) %Original file missing
+        error('Camino:FileNotFound',['The File ''Caminodt.Bdouble'' is missing in the folder ''data/''',...
+            'of the MVDP project.\n Please follow the tutorial at\n',...
+            'http://cmic.cs.ucl.ac.uk/camino//index.php?n=Tutorials.DTI\n',...
+            'and save the file fro step 5 as ''Caminodt.Bdouble'' in ''data/''']);
+    end
+    fid = fopen([dataFolder,'Caminodt.Bdouble'], 'r', 'b');
+    dataFile = fread(fid, 'double');
+    fclose(fid);
+    % Reshape into 8blocks of data
+    data=reshape(dataFile,8,size(dataFile,1)/8);
+    % remove each first 2 to keep just 6 values and reshape to known data size
+    data=data(3:8,:);
+    l=112;
+    m=112;
+    n=50;
+    data = reshape(data,[6,l,m,n]);
+    % create tensor image from data
+    dataDTI  = zeros(3,3,l,m,n);
+    for i=1:l             % 1 2 3    | Ordering of the elements for the matrix
+        for j=1:m         % 2 4 5
+            for k=1:n     % 3 5 6
+                dataDTI(1,1,i,j,k) = data(1,i,j,k);
+                dataDTI(1,2,i,j,k) = data(2,i,j,k);
+                dataDTI(1,3,i,j,k) = data(3,i,j,k);
+                dataDTI(2,1,i,j,k) = data(2,i,j,k);
+                dataDTI(2,2,i,j,k) = data(4,i,j,k);
+                dataDTI(2,3,i,j,k) = data(5,i,j,k);
+                dataDTI(3,1,i,j,k) = data(3,i,j,k);
+                dataDTI(3,2,i,j,k) = data(5,i,j,k);
+                dataDTI(3,3,i,j,k) = data(6,i,j,k);
+            end
+        end
+    end
+    fn = dataDTI(:,:,rangeX,rangeY,sliceNum);
+    l = length(rangeX); m = length(rangeY);
+    save([resultsFolder,dataName,'.mat'],'fn','l','m');
+end
+if getDebugLevel('LoadData')
+    load([resultsFolder,dataName,'.mat'],'fn','l','m');
+end
+if sum(size(fn))==0
+    error('Both Writing and Loading Data are disabled, no data was loaded.');
+end
+rM = ~permute(all(reshape(fn,9,l,m)==0,1),[2,3,1]);
+if getDebugLevel('Figures')
+    figure(1);
+    plotSPD(fn/max(fn(:)),'GridDistance',0.25,'EllipsoidPoints',20);
+    title('Original data.');
+    figure(2);
+    plotSPD(fn(:,:,subRangeX,subRangeY)/max(fn(:)),'GridDistance',0.25,'EllipsoidPoints',20);
+    title('Part of the original data.');
+    pause(0.02);
+end
+%Export original data
+if getDebugLevel('WriteImages')
+    exportSPD2Asymptote(fn/max(fn(:)),'File',[resultsFolder,name,'-original.asy'],'GridDistance',0.25,'ExportHeader',true);
+    exportSPD2Asymptote(fn(:,:,subRangeX,subRangeY)/max(fn(:)),'File',[resultsFolder,name,'-original-sub.asy'],'GridDistance',0.25,'ExportHeader',true);
+end
+%
+%
+%% General Parameters for all problems
+M = SymPosDef(3);
+problem.M = M;
+problem.f = fn;
+problem.MaxIterations = 800;
+problem.Epsilon = 10^(-5);
+problem.lambda = pi/2;
+problem.M.steps = 5; %just do 10 gradient descent steps
+problem.RegMask = rM; %only regularize nonzero items
+problem.alpha = 0.05;
+problem.beta = 0.1;
+if getDebugLevel('logfile') %display parameters in logfile
+    problem     %#ok<NOPTS>
+    problem.M   
+end
+%% CPPA
+fr = cppa_ad_2D(problem);
+%% Export Results
+if getDebugLevel('WriteImages')>0
+    fileStr = [resultsFolder,name,'-p-',num2str(problem.alpha),'-',num2str(problem.beta)];
+    fileStr(fileStr=='.') = [];
+    exportSPD2Asymptote(fr/max(fr(:)),'File',[fileStr,'.asy'],'GridDistance',0.25,'ExportHeader',true);
+    exportSPD2Asymptote(fr(:,:,subRangeX,subRangeY)/max(fr(:)),'File',[fileStr,'sub.asy'],'GridDistance',0.25,'ExportHeader',true);
+    save([fileStr,'.mat'],'fr')
+end
+if getDebugLevel('Figures')
+    figure(3);
+    plotSPD(fr/max(fr(:)),'GridDistance',0.25,'EllipsoidPoints',20);
+    title(['Denoised Data. Parameters: \alpha=',num2str(problem.alpha),' \beta=',num2str(problem.beta),'.']);
+    figure(4)
+    plotSPD(fr(:,:,subRangeX,subRangeY)/max(fr(:)),'GridDistance',0.25,'EllipsoidPoints',20);
+    title(['Denoised part. Parameters: \alpha=',num2str(problem.alpha),' \beta=',num2str(problem.beta),'.']);
+end
+%% End logfile
+if getDebugLevel('logfile')
+    disp([' --- Logfile of Experiment ',name,'; ended ',datestr(datetime),' ---']);
+    diary off;
+end
---
+++ helpers/exportSPD2Asymptote.m
@@ -0,0 +1,186 @@
+function [dataStr,gD] = exportSPD2AsymptoteTry(varargin)
+% dataStr = exportSPD2Asymptote(f) - generate a File for a POVRay plot a given
+%          1D, 2D or 3D signal of symmetric positive definite matrices.
+%
+% INPUT
+%   f : given (1D or 2D) data of symmetric positive definite matrices, i.e.
+%       an 3x3xImageSize-d aray
+%
+% OPTIONAL
+%   'Colors'          : ([]) provide a color data image, i.e. an array
+%       of the same size of the data ImageSizex3 providing color data for
+%       each data item.
+%   'ColorScheme'     : ('GA') values to be used for coloring:
+%       'GA' [G]eometric [A]nisotropy, 'FA' [F]ractional [A]nisotropy,
+%       'Direction' for the directional encoding. This values is ignored
+%       if a color image (see previous Option) is provided
+%   'EllipsoidPoints' : (6) number of points used to surf eny ellipse. The
+%       standard value does not produce that nice results, but scales up to
+%       at least 112^2; for smaller images use a larger value, e.g. 15.
+%   'File'            : (String) Write the generated Code directly into a file
+%   'ExportHeader'    : (false) whether to export the Header of the
+%       .asy-File or just the drawing commands.
+%   'GridDistance'    : a value or vector of distances between the center
+%       points of the ellipsoids. Standard Value is 1. If it is set to [],
+%       the maximal axis is set to sqrt(3)*a, where a is the maximal
+%       eigenvalue of all matrices involved.
+%   'GridScale'       : (1) Scale the Grid additionally in the directions,
+%       which can be used to scale the optimal grid from the previous
+%       option down after its computation
+%
+% OUTPUT
+%   dataStr : the String containing the POV-Ray source code
+% ---
+% Manifold Valued Image Restoration 1.0
+% R. Bergmann ~ 2015-02-24 | 2015-12-05
+
+% Logfile
+% 2015-03-27 Added the optional parameter ExportHeader
+% 2015-04-01 Added GridScale
+ip = inputParser;
+addRequired(ip,'f');
+addParameter(ip,'Colors',[]);
+addParameter(ip,'GridDistance',[]);
+addParameter(ip,'GridScale',1);
+addParameter(ip,'ColorScheme', 'GA');
+addParameter(ip,'ExportHeader',false);
+addParameter(ip,'File', '');
+parse(ip, varargin{:});
+vars = ip.Results;
+dataDim = size(vars.f);
+manDim = dataDim(1:2);
+assert( all(manDim==[3,3]), 'visualization only works for 3x3 matrices');
+d = manDim(1);
+if (numel(vars.Colors)>0)
+    assert( all(size(vars.Colors)==[dataDim(3:end),3]),'The color data has to be the same as the data dimensions with an additional last dimension with RGB channels');
+end
+dataDim = dataDim(3:end);
+if length(vars.GridDistance)==1
+    gD = vars.GridDistance*ones(1,3);
+elseif isempty(vars.GridDistance)
+    % Compute maximal eigenvalue ... may take some time?
+    allM = reshape(vars.f,3,3,[]);
+    maxEV = 0;
+    for i=1:size(allM,3)
+        maxEV = max(maxEV,max(eig(allM(:,:,i))));
+    end
+    gD = sqrt(length(dataDim))*maxEV*ones(1,3);
+else
+    gD = ones(1,3);
+    gD(1:length(vars.GridDistance)) = vars.GridDistance;
+end
+gD = gD.*vars.GridScale;
+numCol = 256;
+hsvCM = hsv(numCol);
+pt = zeros(1,3);
+if length(dataDim)>2 %2D or 1D
+    pt(3) = dataDim(3);
+end
+if length(dataDim)>1
+    pt(2) = dataDim(2);
+end
+pt(1) = dataDim(1);
+% Header
+if ~isempty(vars.File) %Export given
+    exportFile = fopen(vars.File,'w','n','UTF-8');
+end
+dataStr = '';
+if vars.ExportHeader
+    dataStr = sprintf([...%'\\\\begin{asypicture}{name=',vars.File,'}\n',...
+        '\timport three;\n\timport settings;\n\n',...
+        '\tsurface ellipsoid(triple v1,triple v2,triple v3,real l1,real l2, real l3, triple pos=O) {\n',...
+        '\t\ttransform3 T = identity(4);\n\t\tT[0][0] = l1*v1.x;\n\t\tT[1][0] = l1*v1.y;\n\t\tT[2][0] = l1*v1.z;\n',...
+        '\t\tT[0][1] = l2*v2.x;\n\t\tT[1][1] = l2*v2.y;\n\t\tT[2][1] = l2*v2.z;\n',...
+        '\t\tT[0][2] = l3*v3.x;\n\t\tT[1][2] = l3*v3.y;\n\t\tT[2][2] = l3*v3.z;\n',...
+        '\t\tT[0][3] = pos.x;\n\t\tT[1][3] = pos.y;\n\t\tT[2][3] = pos.z;\n',...
+        '\t\treturn T*unitsphere;\n\t}\n\n',...
+        '\tsize(200);\n\n',...
+        '\treal gDx=',num2str(gD(1)),';\n',...
+        '\treal gDy=',num2str(gD(2)),';\n',...
+        '\treal gDz=',num2str(gD(3)),';\n\n',...
+        '\tcurrentprojection=perspective( camera=(gDx*',num2str((pt(1)-1)/2),',gDy*',num2str((pt(2)-1)/2),',',num2str(max(dataDim)),'),',...
+        'up=Y, target=(gDx*',num2str((pt(1)-1)/2),',gDy*',num2str((pt(2)-1)/2),',gDz*',num2str((pt(3)-1)/2),'));\n']);
+    %         '\tcurrentprojection=perspective( camera=(gDx*',num2str(pt(1)/2),',gDy*',num2str(pt(2)/2),',-',num2str(max(dataDim)),'-2*gDx),',...
+    %         'up=Z, target=(gDx*',num2str(pt(1)/2),',gDy*',num2str(pt(2)/2),',gDz*',num2str(pt(3)/2),'));']);
+end
+if ~isempty(vars.File) %Export given
+    fprintf(exportFile,dataStr);
+end
+for i=1:prod(dataDim) %run through all matrices
+    [x,y,z] = ind2sub(dataDim,i); %position on grid, works for all 3 dimension (1,2,3 dim data)
+    if all(all(~isnan(vars.f(:,:,x,y,z))))
+        [V,D] = eig(vars.f(:,:,x,y,z));
+        lambda = diag(D);
+        if numel(vars.Colors)>0
+            switch length(size(dataDim)>1)
+                case 1
+                    color = vars.Colors(x,:);
+                case 2
+                    color = vars.Colors(x,y,:);
+                case 3
+                    color = vars.Colors(x,y,z,:);
+            end
+        else
+        switch vars.ColorScheme
+            case 'GA'
+                if all(all(vars.f(:,:,x,y,z)==0))
+                    color=hsvCM(1,:);
+                else
+                    AR = sqrt(((d-1)/d)*sum( log(lambda).^2) - 2/d*sum(sum(tril(log(lambda)*log(lambda)',-1))));
+                    ARnormed = AR/(1+AR); %See Also Moakher & Batchelor in [0,1]
+                    colInd = round(ARnormed*(numCol-1))+1; %color index
+                    if ~isreal(colInd) || isnan(colInd) %might happen due to rounding errors
+                        lambda = zeros(1,3);
+                        color = hsvCM(1,:);
+                    else
+                        color = hsvCM(colInd,:);
+                    end
+                end
+            case 'FA'
+                if all(all(vars.f(:,:,x,y,z)==0))
+                    color=hsvCM(1,:);
+                else
+                    AF = sqrt(((d-1)/d)*sum( lambda.^2) - 2/d*sum(sum(tril(lambda*lambda',-1))));
+                    AFnormed = AF/norm(vars.f(:,:,x,y,z),'fro'); %See Also Moakher & Batchelor in [0,1]
+                    colInd = round(AFnormed*(numCol-1))+1; %color index
+                    if ~isreal(colInd) || isnan(colInd) %might happen due to rounding errors
+                        lambda = zeros(1,3);
+                        color = hsvCM(1,:);
+                    else
+                        color = hsvCM(colInd,:);
+                    end
+                end
+            case 'Direction'
+                color = V(:,3) / max(abs(V(:,3)));
+                color = abs(color);
+            otherwise
+                error('Unknown ColorScheme; available Schemes are ''GA'', ''FA'', and ''Direction''.');
+        end
+        end
+        if ~all(all(vars.f(:,:,x,y,z)==0)) % only export nonzero ones for computational efforts
+            line = ['\t\tdraw(\n\t\t\tellipsoid( ',...
+                '(',num2str(V(1,1)),',',num2str(V(2,1)),',',num2str(V(3,1)),'), ',...
+                '(',num2str(V(1,2)),',',num2str(V(2,2)),',',num2str(V(3,2)),'), ',...
+                '(',num2str(V(1,3)),',',num2str(V(2,3)),',',num2str(V(3,3)),'), ',...
+                num2str(lambda(1)),', ',num2str(lambda(2)),', ',num2str(lambda(3)),', ',...
+                '(gDx*',num2str(x-1),',gDy*',num2str(y-1),',gDz*',num2str(z-1),')),',...
+                'rgb(',num2str(color(1)),',',num2str(color(2)),',',num2str(color(3)),'));\n'];
+            if ~isempty(vars.File) %Export given
+                fprintf(exportFile,line);
+            end
+            if nargout>0
+                dataStr = sprintf([dataStr,line]);
+            end
+        end
+    end
+%    if round(100*(i-1)/prod(dataDim))~=round(100*(i)/prod(dataDim))
+%        disp([' ',num2str(round(100*(i)/prod(dataDim))),'%']);
+%    end
+end
+if ~isempty(vars.File) %Export given
+    fclose(exportFile);
+end
+if nargout==0
+    clear dataStr;
+end
+end
