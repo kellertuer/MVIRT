b63ca7fa0b6ce92e7a34faf0c8010f3424e71750
Ronny Bergmann <mail@rbergmann.info>
2018-02-10 11:17:10 +0100

    adds a stepsizerule function for the Armijo step size.

---
+++ helpers/stepSizeArmijo.m
@@ -0,0 +1,37 @@
+function stepsize = stepSizeArmijo(varargin)
+
+ip = inputParser();
+addRequired(ip,'M', @(x) validateattributes(x,{'manifold'},{}))
+addRequired(ip,'F');
+addRequired(ip,'x');
+addRequired(ip,'eta');
+addOptional(ip,'Gradient',[]);% if not set assume eta = grad
+addOptional(ip,'InitialStepsize',1);
+addOptional(ip,'rho',0.5);
+addOptional(ip,'c',0.0001);
+parse(ip, varargin{:});
+vars = ip.Results;
+if isempty(vars.Gradient)
+    Gradient = vars.eta;
+elseif all(size(vars.Gradient) == size(vars.eta))
+    Gradient = vars.Gradient;
+else
+    error('Gradient and descent direction have to be of same dimension');
+end
+
+e=vars.F(vars.x);
+stepsize = vars.InitialStepsize;
+e_n = e-1;
+while e > e_n
+    x_n = vars.M.exp(vars.x,-vars.eta,stepsize);
+    e_n = vars.F(x_n)+vars.c*stepsize*sum(...
+        vars.M.dot(vars.x(vars.M.allDims{:},:),vars.eta(vars.M.allDims{:},:),Gradient(vars.M.allDims{:},:)));
+    stepsize = stepsize/vars.rho;
+end
+stepsize = stepsize*vars.rho;
+while e < e_n && stepsize > eps
+    stepsize = stepsize*vars.rho;
+    x_n = vars.M.exp(vars.x,-vars.eta,stepsize);
+    e_n = vars.F(x_n)+vars.c*stepsize*sum(...
+        vars.M.dot(vars.x(vars.M.allDims{:},:),vars.eta(vars.M.allDims{:},:),Gradient(vars.M.allDims{:},:)));
+end
