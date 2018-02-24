f92a96e1b2f6b76a2cd68bbf008bf8fd687ed84c
Ronny Bergmann <mail@rbergmann.info>
2018-02-24 14:28:19 +0100

    finishes SpiralInpaint rework.

--- examples/S1/SpiralInpaint.m
+++ examples/S1/SpiralInpaint.m
@@ -30,14 +30,9 @@ run('../../initMVIRT.m')
 %
 %
 %% Settings
-setDebugLevel('LevelMin',0);        % Minimal Value of all global Values
-setDebugLevel('LevelMax',100);      % Maximal Value ...
-setDebugLevel('text',3);            % quite verbose text, but...
-setDebugLevel('IterationStep',100); % only every 100th iteration
-setDebugLevel('WriteImages',1);     % 0: no file writing, 1: file writing
-setDebugLevel('time',3);            % verbose time
-setDebugLevel('Figures',1);         % 0: no figure display, 1: figures are displayed
-setDebugLevel('logfile',1);         % 0: no logfile 1: logfile
+useLogfile  = true;
+showFigures = true;
+writeImages = true;
 
 dataFolder = ['..',filesep,'..',filesep,'data',filesep];
 folder = ['examples',filesep,'S1',filesep];
@@ -46,7 +41,7 @@ name = 'PhaseSpiral';
 load([dataFolder,name,'.mat']);
 
 %% Logfile?
-if getDebugLevel('logfile')
+if useLogfile
     clc
     if exist([resultsFolder,name,'.log'],'file')
         delete([resultsFolder,name,'.log'])
@@ -55,13 +50,13 @@ if getDebugLevel('logfile')
     disp([' --- Logfile of Experiment ',name,' started ',datestr(datetime),' ---']);
 end
 
-if getDebugLevel('Figures')
+if showFigures
     figure(1); imagesc(spiral,[-pi,pi]); colormap(hsv(1024));
     title(['Original Data ''',name,'''.']);
     axis image
     axis off
 end
-if getDebugLevel('WriteImages')
+if writeImages
     Vexp = uint8((spiral+pi)/(2*pi)*255);
     %Export original image
     imwrite(Vexp,colormap(hsv(255)),[resultsFolder,name,'.png'],'png');
@@ -78,14 +73,14 @@ M(:,1:3:size(spiral,2)) = 0;
 M(:,2:3:size(spiral,2)) = 0;
 M = M>0; %binarize
 img = spiral.*M; %Just loose data
-if getDebugLevel('Figures')
+if showFigures
     figure(2); imagesc(M,[0,1]); colormap(gray); axis square
     pr = 100 * sum(~M(:))/length(M(:));
     title(['Mask: White pixels are kept, black ones are destroyed. (',num2str(pr,3),'% destroyed).']);
     figure(3); imagesc(img,[-pi,pi]);  colormap(hsv(1024)); axis square
     title('input data with destroyed points');
 end
-if getDebugLevel('WriteImages')
+if writeImages
     % white mask of cut out (rest transparent
     imwrite(uint8(255*(1-M)),[resultsFolder,name,'-mask-w.png'],'Alpha',255*(1-M));
     % black mask of cut out (rest transparent
@@ -105,7 +100,7 @@ tic
 VresTV2 = permute(CPP_AdditiveTV12(problem),[2,3,1]);
 toc
 
-if getDebugLevel('Figures')
+if showFigures
     figure(4); imagesc(VresTV2,[-pi,pi]); colormap(hsv(1024));axis image; axis off;
     title(['Result of the reconstruction using \alpha=',num2str(problem.alpha(1),4),' and \beta=',num2str(problem.beta(1),4),'.']);
 end
@@ -113,11 +108,11 @@ problem.beta = zeros(2);
 tic
 VresTV = permute(CPP_AdditiveTV12(problem),[2,3,1]);
 toc
-if getDebugLevel('Figures')
+if showFigures
     figure(5); imagesc(VresTV,[-pi,pi]); colormap(hsv(1024));axis image; axis off;
     title(['Result of the reconstruction using \alpha=',num2str(problem.alpha(1),4),' and \beta=0.']);
 end
-if getDebugLevel('WriteImages')
+if writeImages
     Vresexp = uint8((VresTV2+pi)/(2*pi)*255);
     imwrite(Vresexp,colormap(hsv(255)),[resultsFolder,name,'-impaintedTV12.png'],'png');
     Vresexp = uint8((VresTV+pi)/(2*pi)*255);
@@ -128,18 +123,18 @@ VDistTV12 = problem.M.dist(permute(spiral,[3,1,2]),permute(VresTV2,[3,1,2]));
 METV12 = sum(sum(VDistTV12.^2))/length(spiral(:));
 VDistTV = problem.M.dist(permute(spiral,[3,1,2]),permute(VresTV,[3,1,2]));
 METV = sum(sum(VDistTV.^2))/length(spiral(:));
-if getDebugLevel('Figures')
+if showFigures
     figure(6); imagesc(VDistTV12); colormap(flipud(gray(1024))); axis image; axis off;    title(['Reconstruction Error TV1&2. MSE = ',num2str(METV12),'.']);
     figure(7); imagesc(VDistTV); colormap(flipud(gray(1024))); axis image; axis off;    title(['Reconstruction Error TV. MSE = ',num2str(METV12),'.']);
 end
-if getDebugLevel('WriteImages')
+if writeImages
     VDistexp = uint8((VDistTV12-min(min(VDistTV12)))/(max(max(VDistTV12))-min(min(VDistTV12)))*255);
     imwrite(VDistexp,colormap(flipud(gray(255))),[resultsFolder,name,'-impainting-TV12-error.png'],'png');
     VDistexp = uint8((VDistTV-min(min(VDistTV)))/(max(max(VDistTV))-min(min(VDistTV)))*255);
     imwrite(VDistexp,colormap(flipud(gray(255))),[resultsFolder,name,'-impainting-TV-error.png'],'png');
 end
 %% End logfile
-if getDebugLevel('logfile')
+if useLogfile
     disp([' --- Logfile of Experiment ',name,' ended ',datestr(datetime),' ---']);
     diary off;
 end
