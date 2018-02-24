34a52636acf6f03dfa8c605b532af5ac1937600c
Ronny Bergmann <mail@rbergmann.info>
2018-02-24 12:30:22 +0100

    finishes Huber Code.

--- examples/S1/Huber.m
+++ examples/S1/Huber.m
@@ -25,18 +25,10 @@ run('../../initMVIRT.m')
 %
 %
 %% Settings
-setDebugLevel('LevelMin',0);
-setDebugLevel('LevelMax',1000);
-setDebugLevel('text',3); %verbose, but...
-setDebugLevel('IterationStep',1000); %only every 50th iteration
-setDebugLevel('time',2);
-% The following two variables do the following
-% LoadData: If this is set to 1,
-setDebugLevel('LoadData',1); %0: generate new data 1: load existing data (if it exists), (other wise it is generated)
-setDebugLevel('WriteData',0); %0: do not write data to file 1: write data to file (overwrites last experiment data!)
-setDebugLevel('Figures',1); %0: no figure display, 1: figures are displayed (disable e.g. for cluster/console work)
-setDebugLevel('logfile',1); %0: no logfile 1: logfile
-format compact
+loadData = true;
+writeData = false;
+showFigures = true;
+useLogfile = true;
 %
 %
 % Strings (only change these if you're knowing what you do
@@ -49,7 +41,7 @@ name = ['S1-HuberN',num2str(N)];
 dataName = ['S1-HuberN',num2str(N)];
 %
 % Logfile?
-if getDebugLevel('logfile')
+if useLogfile
     clc
     if exist([resultsFolder,name,'.log'],'file')
         delete([resultsFolder,name,'.log'])
@@ -57,13 +49,13 @@ if getDebugLevel('logfile')
     diary([resultsFolder,name,'.log']);
     disp([' --- Logfile of Experiment ',name,' started ',datestr(datetime),' ---']);
 end
-if getDebugLevel('WriteData') % Create new data
+if writeData % Create new data
     [fn,fo,t] = CyclicPiecewiseSignal(N,sigma);
     save([resultsFolder,dataName,'.mat'],'fn','fo','t','sigma');
-elseif getDebugLevel('LoadData')
+elseif loadData
     load([resultsFolder,dataName,'.mat'],'fn','fo','t','sigma');
     metaData = dir([resultsFolder,dataName,'.mat']);
-    debug('text',3,'Text',['Using File Data generated ',datestr(metaData.date),'.']);
+    disp(['Using File Data generated ',datestr(metaData.date),'.']);
     N = length(fn);
 else
     error('Either Loading or Creating(Wirting) Data must be set');
@@ -94,7 +86,7 @@ frTV12 = CPP_AdditiveTV12(problem);
 toc
 
 MSETV = sum(M.dist(fo,frTV12).^2)*1/N;
-if getDebugLevel('Figures')
+if showFigures
     figure(2); plot(t,fo,'.b',t,frTV12,'.k');
     ylim([-pi,pi]);
     title(['TV1&2 Minimization by CPPA, alpha=',num2str(alpha),', beta=',num2str(beta),' MSE=',num2str(MSETV)]);
@@ -105,12 +97,12 @@ problemH.alpha = 1/2;
 problemH.tau = 4*sqrt(2);
 problemH.omega = pi/16;
 problemH.f = fn;
-problemH.stoppingCriterion = stopCritMaxIterEpsilonCreator(problem.M,4000,0);
+problemH.stoppingCriterion = stopCritMaxIterEpsilonCreator(problem.M,iter,epsilon);
 problemH.lambdaIterate = @(iter) pi/iter;
 frH = CPP_HuberTV(problemH);
 
 MSEH = sum(M.dist(fo,frH).^2)*1/N;
-if getDebugLevel('Figures')
+if showFigures
     figure(3); plot(t,fo,'.b',t,frH,'.k');
     ylim([-pi,pi]);
     title(['Huber-type TV Minimization by CPPA, alpha=',num2str(problemH.alpha),', tau=',num2str(problemH.tau),', omega=',num2str(problemH.omega),' MSE=',num2str(MSETV)]);
