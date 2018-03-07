3d477f0c901776c2e8e1aeb34c3b19af553620e3
Ronny Bergmann <mail@rbergmann.info>
2018-03-07 10:47:36 +0100

    Implements parallelTransport, TpONB and addNoise in Hn

--- manifolds/Hn.m
+++ manifolds/Hn.m
@@ -1,6 +1,6 @@
 classdef Hn < manifold & handle
     % The n-dimensional hyperbolic manifold H(n) embedded in R^{n+1}
-    %    
+    %
     % ---
     % Manifold-Valued Image Restoration Toolbox 1.0, R. Bergmann ~2015-10-20
     properties
@@ -34,12 +34,13 @@ classdef Hn < manifold & handle
             % OUTPUT
             %   y : resulting point(s) on Hn
             % ---
-            % Manifold-Valued Image Restoration Toolbox 1.1, R. Bergmann ~ 2015-01-20 | 2015-10-20
+            % Manifold-Valued Image Restoration Toolbox 1.
+            %  R. Bergmann ~ 2015-01-20 | 2015-10-20
             if nargin<4 % define t
                 t=1;
             end
             if this.useMex
-                y = HnExp(x,xi,t);                
+                y = HnExp(x,xi,t);
             else
                 y = this.localExp(x,xi,t);
             end
@@ -57,7 +58,7 @@ classdef Hn < manifold & handle
             % ---
             % Manifold-Valued Image Restoration Toolbox 1.1
             % R. Bergmann ~ 2015-10-20
-
+            
             % computes v = (y+x*<x,y>)/sqrt(<x,y>^-1)*d(x,y)
             if this.useMex
                 xi = HnLog(x,y);
@@ -82,13 +83,13 @@ classdef Hn < manifold & handle
             % OPTIONAL
             % 'Weights' : (1/n*ones([m,n]) 1xn or mxn weights for the mean
             %            the first case uses the same weights for all means
-            % 'InitVal' : m Initial Data points for the gradient descent 
+            % 'InitVal' : m Initial Data points for the gradient descent
             % 'MaxIterations': Maximal Number of Iterations
             % 'Epsilon'      : Maximal change before stopping
             %
             %
             % Manifold-Valued Image Restoration Toolbox 1.1, J. Persch 2015-11-05
-           
+            
             ip = inputParser;
             addRequired(ip,'f');
             addParameter(ip,'Weights',NaN);
@@ -123,7 +124,7 @@ classdef Hn < manifold & handle
                 if any(size(w) ~= [m,n])
                     error('dim w do not match data points');
                 end
-            end          
+            end
             if vars.Epsilon > 0
                 epsilon = vars.Epsilon;
             else
@@ -138,9 +139,9 @@ classdef Hn < manifold & handle
             end
             if ~this.useMex
                 x=this.mean@manifold(f,'Weights',w,'Epsilon',epsilon,'MaxIterations',iter);
-            else            
-            x = HnMean(f,w,epsilon,iter);
-            end            
+            else
+                x = HnMean(f,w,epsilon,iter);
+            end
         end
         function d = dist(this,x,y)
             % d = dist(x,y) distance between points x and y on Hn
@@ -173,18 +174,92 @@ classdef Hn < manifold & handle
             % ---
             % Manifold-Valued Image Restoration Toolbox 1.1
             % R. Bergmann ~ 2015-10-20
-            if nargin < 4 
+            if nargin < 4
                 nu = xi;
                 xi = x;
             end
             if this.useMex
                 ds = HnDot(xi,nu);
             else
-                ds = this.localDot(xi,nu);
+                ds = this.localDot(x,xi,nu);
             end
         end
-        function addNoise(~,~,~)
-            error('This method is not yet implemented.');
+        function eta = parallelTransport(this,x,y,xi)
+            % eta = parallelTransport(x,y,xi) parallel transport xi along g(.,x,y)
+            %
+            % INPUT
+            %    x : a point( set) on P(m)
+            %    y : a point( set) on P(m)
+            %   xi : a tangential vector( set, one) at (each) X
+            %
+            % OUTPUT
+            %  eta : tangential vector( set, one) at (each) Y
+            %
+            % ---
+            % Manifold-Valued Image Restoration Toolbox 1.
+            %  R. Bergmann ~ 2018-03-05 | 2018-03-05
+            
+            % Note that this is very similar -- again 
+            sXi = size(xi);
+            dir = this.log(x,y);
+            norms = sqrt(this.dot(x,dir,dir));
+            dir = dir./shiftdim(norms,-1);
+            normMask = repmat(norms==0,[this.ItemSize,ones(1,length(sXi(2:end)))]);
+            scp = this.dot(x,dir,xi);
+            % substract V-part (scp*dir) and add the negative direction
+            eta = xi - shiftdim(scp,-1).*(dir+this.log(y,x)./shiftdim(norms,-1));
+            eta(normMask) = xi(normMask); %those that need not to be transported
+        end
+        function Xi = TpMONB(this,x,y)
+            % Xi = TpMONB(x,y)
+            % Compute an ONB in TpM, where the first vector points to y,
+            % whin given.
+            %
+            % INPUT
+            %     x : base point( sets)
+            % OPTIONAL:
+            %     y : directional indicator( sets) for the first vector(s).
+            %
+            % OUTPUT
+            %    Xi : basiscolumn matrice(s)
+            % ---
+            % MVIRT 1.0, R. Bergmann ~ 2014-10-19 | 2014-10-23
+            if isrow(x)
+                x_=x';
+            else
+                x_=x;
+            end
+            y_given = 0;
+            id = eye(this.ItemSize);
+            Xi = permute(repmat(id(:,1:end-1),1,1,size(x_,2)),[1,3,2]);
+            if nargin == 3
+                y_given =1;
+                if isrow(y)
+                    y_=y';
+                else
+                    y_=y;
+                end
+                Xi(:,:,1) = this.log(x_,y_);
+                normsv = sqrt(sum(Xi(:,:,1).^2,1));
+                Xi(:,normsv>eps,1) = Xi(:,normsv>eps,1)./repmat(normsv(normsv>eps),[this.ItemSize,1,1]);
+            end
+            for col=1:size(Xi,2)
+                for i = 1+y_given:this.Dimension
+                    Xi(:,:,i) = Xi(:,:,i) - x.*repmat(permute(this.dot(Xi(:,:,i),x,Xi(:,:,i))./this.dot(x,x,x),[2,1]),this.ItemSize,1,1);
+                    for j = 1:i-1
+                        Xi(:,:,i) = Xi(:,:,i) - Xi(:,:,j).*repmat(permute(this.dot(Xi(:,:,i),Xi(:,:,j),Xi(:,:,i)),[2,1]),this.ItemSize,1,1);
+                    end
+                    Xi(:,:,i) = Xi(:,:,i)./sqrt(repmat(permute(this.dot(Xi(:,:,i),Xi(:,:,i),Xi(:,:,i)),[2,1]),this.ItemSize,1,1));
+                end
+            end
+        end
+        function y = addNoise(this,x,sigma)
+        % y = addNoise(x,sigma) add noise 
+            sizes = size(x);
+            x = reshape(x,this.ItemSize,[]);
+            ONB = this.TpMONB(x);
+            y = this.exp(x,sum(repmat(sigma*randn(1,size(x,2),this.Dimension),this.ItemSize,1,1).*ONB,3));
+            y = reshape(y,sizes);
         end
     end
     methods (Access = private)
@@ -207,8 +282,8 @@ classdef Hn < manifold & handle
         end
         function V = localLog(this,X,Y)
             dims = size(X);
-                X_ = reshape(X,this.ItemSize,[]);
-                Y_ = reshape(Y,this.ItemSize,[]);
+            X_ = reshape(X,this.ItemSize,[]);
+            Y_ = reshape(Y,this.ItemSize,[]);
             scp = this.dot(X_,Y_).';
             w = Y_ + X_.*repmat(scp,[this.ItemSize,1]);
             normw = sqrt(max(this.dot(X_,Y_).^2-1,0)).';
@@ -233,22 +308,19 @@ classdef Hn < manifold & handle
             d = zeros(prod(dims(2:end)),1);
             cols = ~(max(abs(X-Y))<eps);
             if any(cols)
-                d(cols) = acosh(max(1,-this.dot(X(:,cols),Y(:,cols))));  
+                d(cols) = acosh(max(1,-this.dot(X(:,cols),Y(:,cols))));
             end
             if ~isscalar(dims(2:end))
                 d = reshape(d,dims(2:end));
             end
         end
-        function ds = localDot(this,V,W)
-            dims = size(V);
-            if length(dims)==2
-                ds =  ([ones(1,this.ItemSize-1),-1]*(V.*W)).';
-            else
-                ds = reshape(...
-                     [ones(1,this.ItemSize-1),-1]*...
-                         (reshape(V,this.ItemSize,[]).*reshape(W,this.ItemSize,[]))...
-                     ,dims(2:end));
-            end
+        function ds = localDot(this,~,xi,nu)
+            dims = size(xi);
+            dataDims = dims(2:end);
+            p = [ones(this.ItemSize-1,1);-1]; %prefactors in inner product
+            ds = reshape(...
+               sum(p.*(xi(this.allDims{:},:).*nu(this.allDims{:},:)),1),...
+                [dataDims,1]);
         end
     end
 end
